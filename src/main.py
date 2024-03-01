05ecbd6e435a90ef56c2de92e7efd5070b6a5425
RoJoSaVs <ronnyjosv259@outlook.es>
2024-02-29 20:05:36 -0600

    Test with depth estimation image sample

--- src/main.py
+++ src/main.py
@@ -1,35 +1,18 @@
+import imageHandler
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
- 
-# Define the parameters of the Möbius strip
-R = 2
- 
-# Define the Möbius strip surface
-u = np.linspace(0, 2*np.pi, 100)
-v = np.linspace(-1, 1, 100)
-u, v = np.meshgrid(u, v)
-x = (R + v*np.cos(u/2)) * np.cos(u)
-y = (R + v*np.cos(u/2)) * np.sin(u)
-z = v * np.sin(u/2)
- 
-# Create the plot
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
- 
-# Plot the Möbius strip surface
-ax.plot_surface(x, y, z, alpha=0.5)
- 
-# Set plot properties
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-ax.set_title('Möbius Strip')
- 
-# Set the limits of the plot
-ax.set_xlim([-3, 3])
-ax.set_ylim([-3, 3])
-ax.set_zlim([-3, 3])
- 
-# Show the plot
-plt.show()
+
+
+PATH = 'image-source/depth-estimation-dog.png'
+
+# Original image set as a Matrix
+numpyArray = imageHandler.imageToArray(PATH)
+valueMatrix = imageHandler.imageArrayIterator(numpyArray)
+
+# Matrix to set the depth of each pixel
+indexesMatrix = imageHandler.highestToIndex(valueMatrix)
+
+pcdArray = []
+for rowIndex in range(0, len(indexesMatrix)):
+    for columnIndex in range(0, len(indexesMatrix[rowIndex])):
+        pcdArray.append([rowIndex + 1, columnIndex + 1, indexesMatrix[rowIndex][columnIndex]])
