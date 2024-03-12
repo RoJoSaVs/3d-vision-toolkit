ae790c471f8829463182c4051227d7c71551ecb3
RoJoSaVs <ronnyjosv259@outlook.es>
2024-03-11 21:07:50 -0600

    File to parse an image to its pcd file

---
+++ src/pcd_estimator/mainSample.py
@@ -0,0 +1,32 @@
+import sys
+import numpy as np
+
+# Local imports
+import pcdEstimator as pcdE
+sys.path.append('./src')
+import image_handler_module.imageIO as imageIO
+import pcd_handler_module.pcdFileHandler as pcdFileHandler
+
+
+filename = 'street.png'
+
+# Load the original image
+imageSourcePath = './image-source/'
+streetOriginal = imageIO.readImage(imageSourcePath + filename)
+
+# Load the depth estimated image
+depthImagesPath = './depth-estimation-images/'
+streetDepth = imageIO.readImage(depthImagesPath + filename)
+
+# Converts the image to grayscale
+streetG = imageIO.imageToGrayScale(streetDepth)
+
+# Creates the array with the depth using [x, y, z, rgb]
+positionCalculatorResult = pcdE.positionCalculator(streetOriginal, streetG)
+
+# # Turns the result to a numpy array of: [1 Row] [N columns] [4 numbers each column (x, y, z, rgb)]
+numpyArrayResult = np.asarray(positionCalculatorResult)
+
+# # Stores the numpy array as a pcd file
+filename = './pcd-files/test.pcd'
+pcdFileHandler.pcdFileGenerator(numpyArrayResult, filename)
--- src/pcd_handler_module/pcdFileHandler.py
+++ src/pcd_handler_module/pcdFileHandler.py
@@ -7,15 +7,52 @@ import pypcd4 as pcd4
 # ==========================================================================================
 
 
+# @brief Function that helps to converts a numpy array to a pcd file based on the mode
+# @param numpyArray: Numpy array that will be stored in a pcd-file
+# @param filename: Name of the pcd-file
+# @param auto: Select the function to generate the pcd
+def pcdFileGenerator(numpyArray, filename, auto = False):
+    if(auto):
+        pcdFileGeneratorAuto(numpyArray, filename)
+    else:
+        pcdFileGeneratorManual(numpyArray, filename)
+
+
 # @brief Function that converts a numpy array to an pcd file
 # @param numpyArray: Numpy array that will be stored in a pcd-file
 # @param filename: Name of the pcd-file
-def pcdFileGenerator(numpyArray, filename):
+def pcdFileGeneratorAuto(numpyArray, filename):
     try:
         fields = ('x', 'y', 'z')
         types = (np.float32, np.float32, np.float32)
         pc = pcd4.PointCloud.from_points(numpyArray, fields, types)
         pc.save(filename)
+
+    except NameError:
+        raise NameError
+
+
+# @brief Function that converts a numpy array to an pcd file writing explicit fields
+# @param numpyArray: Numpy array that will be stored in a pcd-file
+# @param filename: Name of the pcd-file
+# @param fields: Fields that will be used in the pcd file ('x', 'y', 'z', 'rgb') by default
+# @param types: Numpy types of each field (numpy.float32) by default
+def pcdFileGeneratorManual(numpyArray, filename):
+    try:
+        # Create the file with each field
+        with open(filename, 'w') as pcdFile:
+            pcdFile.write('# .PCD v0.7 - Point Cloud Data\n')
+            pcdFile.write('VERSION 0.7\n')
+            pcdFile.write('FIELDS x y z rgb\n')
+            pcdFile.write('SIZE 4 4 4 4\n')
+            pcdFile.write('TYPE F F F F\n')
+            pcdFile.write('COUNT 1 1 1 1\n')
+            pcdFile.write('WIDTH {}\n'.format(len(numpyArray)))
+            pcdFile.write('HEIGHT 1\n')
+            pcdFile.write('POINTS {}\n'.format(len(numpyArray)))
+            pcdFile.write('DATA ascii\n')
+            for item in numpyArray:
+                pcdFile.write('{:.6f} {:.6f} {:.6f} {:d}\n'.format(item[0], item[1], item[2], item[3]))
 
     except NameError:
         raise NameError
